f1d2e993eb5836be85d4d166f11837a6ccbc5fe2
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-03-16 14:25:29 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,17 @@ class BirdsController < ApplicationController
     end
   end
 
+  #POST /bird
+  def create 
+    bird = Bird.create( bird_params)
+    render json: bird, status: :created
+  end
+
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 end
